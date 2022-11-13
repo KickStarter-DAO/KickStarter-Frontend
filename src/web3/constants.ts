a2873bae59929fef6cb502f3fd46d8e229ff22c3
Adegbite Ademola Kelvin <Adegbiteademola1999@gmail.com>
2022-11-13 19:53:48 +0100

    contract setup

---
+++ src/web3/constants.ts
@@ -0,0 +1,1 @@
+export const contractAddress:string = "0xF405DE09B71F7FB02200c868D2A3d565b50C5e5D"
---
+++ src/web3/hooks/useContract.ts
@@ -0,0 +1,19 @@
+import { useContract, useProvider, useSigner } from "wagmi"
+import { contractAddress } from "../constants"
+import governanceAbi from "../abi/governanceContract.json"
+
+const useGovernanceContract = () => {
+  const provider = useProvider()
+  const { data: signer } = useSigner()
+  const contract = useContract({
+    address: contractAddress,
+    abi: governanceAbi,
+    signerOrProvider: signer || provider,
+  })
+
+  return {
+    contract,
+  }
+}
+
+export default useGovernanceContract
